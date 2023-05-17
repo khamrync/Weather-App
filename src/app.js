63a47c197ba01b66bea415d0493ce12e02598d8d
khamrync <khamryn.turner@gmail.com>
2023-05-17 09:09:24 -0600

    Weekly forecast fixes

--- src/app.js
+++ src/app.js
@@ -3,13 +3,25 @@ let now = new Date();
 
 let h4 = document.querySelector("h4");
 
-let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 let month = months[now.getMonth()];
 
 let date = now.getDate();
-  
-h4.innerHTML = `Date: ${month} ${date}`;
 
+h4.innerHTML = `Date: ${month} ${date}`;
 
 let current = new Date();
 
@@ -22,15 +34,14 @@ h5.innerHTML = `Time: ${hour}:${minute}`;
 
 // Fahrenheit and Celcius //
 function tempChange(event) {
-
   let temp = document.querySelector(".number");
-  temp.innerHTML = (`82°`)
+  temp.innerHTML = `82°`;
 }
 fahrenheit.addEventListener("click", tempChange);
 
 function tempChange2(event) {
   let temp = document.querySelector(".number");
-  temp.innerHTML = (`27°`)
+  temp.innerHTML = `27°`;
 }
 celsius.addEventListener("click", tempChange2);
 
@@ -38,16 +49,17 @@ celsius.addEventListener("click", tempChange2);
 function displayForecast(response) {
   let forecastElement = document.querySelector("#weather-forecast");
 
-  let days = ["Wed","Thu", "Fri", "Sat", "Sun", "Mon"];
+  let days = ["Wed", "Thu", "Fri", "Sat", "Sun", "Mon"];
   let forecastHTML = `<div class="row align-items-start">`;
-  days.forEach(function (day){
-  forecastHTML = forecastHTML +
-  `
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
     <div class="col">
     <span class="days">${day}</span>
     </div>
     <div class="col">
-      <img class="icons" src="https://openweathermap.org/img/wn/50d@2x.png" width="70%">
+      <img class="icons" src="https://openweathermap.org/img/wn/50d@2x.png" width="42">
     </div>
     <div class="col">
       <span class="max-temp">84°</span> <span class="min-temp">/61°</span> 
@@ -55,10 +67,9 @@ function displayForecast(response) {
   </div>
   </div>
   `;
-  })
+  });
   forecastElement.innerHTML = forecastHTML;
 }
-
 
 // Search engine value //
 function search(event) {
@@ -78,8 +89,8 @@ let apiKey = "2d5e0f0b5a89b155a3d8f0e733b3293a";
 // Coordinates //
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "2d5e0f0b5a89b155a3d8f0e733b3293a";
-  let apiOneCall =`https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperail`;
+  let apiKey = "f81614abe2395d5dfecd45b9298041de";
+  let apiOneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperail`;
   axios.get(apiOneCall).then(displayForecast);
 }
 
@@ -95,13 +106,15 @@ function showTemperature(response) {
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let iconElement = document.querySelector("#weather-icon");
-  iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 
   getForecast(response.data.coord);
 }
 
-
-form.addEventListener("submit", function(event) {
+form.addEventListener("submit", function (event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-text-input");
   let city = searchInput.value;
@@ -109,16 +122,12 @@ form.addEventListener("submit", function(event) {
   axios.get(`${apiURL}`).then(showTemperature);
 });
 
-function displayForecast(response) {
-  let forecastElement = document.querySelector("")
-}
-
 // Current Location Button //
-function showPosition(position){
-  let latitude = position.coords.latitude
+function showPosition(position) {
+  let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
   let apiCall = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
-  axios.get(`${apiCall}`).then(function(response) {
+  axios.get(`${apiCall}`).then(function (response) {
     let temperature = Math.round(response.data.main.temp);
     let temperatureText = document.querySelector(".number");
     temperatureText.textContent = temperature;
@@ -129,9 +138,9 @@ function getCurrentLocation() {
   navigator.geolocation.getCurrentPosition(showPosition);
 }
 
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
   let formButton = document.querySelector("#location");
-  formButton.addEventListener("submit", function(event) {
+  formButton.addEventListener("submit", function (event) {
     event.preventDefault();
     getCurrentLocation();
   });
